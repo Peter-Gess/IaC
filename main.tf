479e6bf4bd617a050fd74b5af8bba7aad93d92fa
Peter-Gess <gesspeter@gmail.com>
2025-06-02 07:23:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAJSIE37KKMHXI3BJQ"
+  secret_key = "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" 
+}  
+
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-tf-test-bucket"
 }
@@ -5,6 +12,6 @@ resource "aws_s3_bucket" "my_bucket" {
 resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   bucket = my-tf-test-bucket.id
   versioning_configuration {
-    status = "Enabled"
+    status = "Suspended"
   }
 }
