ca9242af78f30ccd3f878de2b79fb6c8e1f7baeb
Peter-Gess <gesspeter@gmail.com>
2025-10-15 13:25:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-tf-test-bucket"
 }
-
-resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-  bucket = my-tf-test-bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
